0361708aabb9d4f041cbf0fa1c99897f27ef1f96
Gasbiachraf <achrafgasbi2000@gmail.com>
2024-04-13 23:12:34 +0000

    contact section update

--- src/pages/Home/components/ContactSection.jsx
+++ src/pages/Home/components/ContactSection.jsx
@@ -48,7 +48,7 @@ export const ContactSection = () => {
                         <input value={inputName} onChange={(e) => setInputName(e.target.value)} type="text" placeholder='FullName' className=' bg-transparent placeholder:text-[#36a99a] rounded' name='name' />
                         <input value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} type="email" required placeholder='Email' className=' bg-transparent placeholder:text-[#36a99a] rounded' name='email' />
                         <textarea value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} placeholder='Message ' className='h-[20vh] bg-transparent placeholder:text-[#36a99a] rounded' name='message'></textarea>
-                        <input type="submit"  value="Send" className='bg-[#36a99a] w-[20%] max-[430px]:w-[30%] text-[#032a31]  font-bold px-8 py-2 rounded-full text-xl hover:bg-[#a9cfd0] '/>
+                        <input type="submit"  value="Send" className='bg-[#36a99a] w-[30%]  max-[430px]:px-2 text-[#032a31]  font-bold px-8 py-2 rounded-full text-xl hover:bg-[#a9cfd0] '/>
                     </form>
 
                 </div>
