06f4b0b78599ff62245954d89d511e301f75e263
Gasbiachraf <achrafgasbi2000@gmail.com>
2024-04-14 16:43:21 +0100

    update footer

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -16,12 +16,13 @@ export const CompenentFooter = () => {
                 </div>
                 <div className='flex justify-center pb-8'>
                     <div className=' flex justify-around max-[430px]:justify-evenly w-[10%] pb-4 pt-2 max-[430px]:w-[35%] text-xl '>
-                        <Link target='_blank' to="https://www.linkedin.com/in/achraf-gasbi-02210823a/"><ImLinkedin className='rounded-full hover:text-[#36a99a] duration-200 ' /></Link>
-                        <Link target='_blank' to="https://github.com/Gasbiachraf"><FaGithub className='rounded-full hover:text-[#36a99a] duration-200 ' /></Link>
-                        <Link target='_blank' to="https://twitter.com/gasbi_achraf"><FaXTwitter className='rounded-full bg-[#a9cfd0] text-[#032a31] p-1 hover:bg-[#36a99a] duration-200' /></Link>
-                        <Link target='_blank' to="https://www.instagram.com/achraf_gasbi/"><FaInstagram className='rounded-full bg-[#a9cfd0] text-[#032a31] p-1 hover:bg-[#36a99a] duration-200 ' /></Link>
-                        <Link target='_blank' to="https://in.pinterest.com/achrafgasbi/"><FaPinterest className='rounded-full hover:text-[#36a99a]  duration-200' /></Link>
+                        <a target='_blank' href="https://github.com/Gasbiachraf"><FaGithub className='rounded-full hover:text-[#36a99a] duration-200 ' /></a>
+                        <a target='_blank' href="https://www.linkedin.com/in/achraf-gasbi-02210823a/"><ImLinkedin className='rounded-full hover:text-[#36a99a] duration-200 ' /></a>
+                        <a target='_blank' href="https://twitter.com/gasbi_achraf"><FaXTwitter className='rounded-full bg-[#a9cfd0] text-[#032a31] p-1 hover:bg-[#36a99a] duration-200' /></a>
+                        <a target='_blank' href="https://www.instagram.com/achraf_gasbi/"><FaInstagram className='rounded-full bg-[#a9cfd0] text-[#032a31] p-1 hover:bg-[#36a99a] duration-200 ' /></a>
+                        <a target='_blank' href="https://in.pinterest.com/achrafgasbi/"><FaPinterest className='rounded-full hover:text-[#36a99a]  duration-200' /></a>
                     </div>
+                    <a href=""></a>
                 </div>
             </footer>
 
