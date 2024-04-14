10a4394f5ced2244031ab6fbf8653b469e835b1b
Gasbiachraf <achrafgasbi2000@gmail.com>
2024-04-14 16:56:02 +0100

    update error page

--- src/pages/Error/components/firstSection.jsx
+++ src/pages/Error/components/firstSection.jsx
@@ -3,7 +3,7 @@ import './firstSection.sass'
 
 export const FirstSectionError = () => {
     return (
-        <div className='pt-16 flex justify-center items-center bg-[#032a31] text-[#a9cfd0] h-[80vh] text-6xl'>
+        <div className='pt-16 flex justify-center items-center bg-[#032a31] text-[#a9cfd0] h-[80vh] text-6xl max-[430px]:text-3xl'>
 
             <p className=''>Oups!................ERROR 404 </p>
             
