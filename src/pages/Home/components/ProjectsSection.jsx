203ab94e93a7574394c5454f5a58bfe004374e86
Gasbiachraf <gasbi.achraf@gmail.com>
2024-08-19 16:44:47 +0100

    section product x3

--- src/pages/Home/components/ProjectsSection.jsx
+++ src/pages/Home/components/ProjectsSection.jsx
@@ -3,6 +3,7 @@ import currency from '../../../assets/img/currency.png'
 import shopSite from '../../../assets/img/shopSite.png'
 import docteur from '../../../assets/img/docteur.png'
 import babytoys from '../../../assets/img/babytoys.png'
+import evenext from '../../../assets/img/Evenext.png'
 import { useState } from 'react'
 
 import { Card } from 'flowbite-react';
@@ -11,31 +12,10 @@ import { Card } from 'flowbite-react';
 export const ProjectsSection = () => {
     const [projects, setProjects] = useState([
         {
-            paragraph: 'Explore our varied online selection of trendy, high-quality products to suit every style and budget.',
-            titre: 'Store website',
-            img: shopSite,
-            link : "https://storefash.vercel.app/",
-
-        },
-        {
-            paragraph: 'Medical practice dedicated to your health, offering personalized consultations and attentive care.',
-            titre: 'Medical practice website',
-            img: docteur,
-            link : "https://mediplusag.netlify.app/",
-
-        },
-        {
-            paragraph: 'Explore the selection of delicious dishes online and discover the creators of these culinary masterpieces.',
-            titre: 'Restaurant website',
-            img: resto,
-            link : "https://lionsresto.netlify.app/",
-
-        },
-        {
-            paragraph: 'Discover current cryptocurrency trends with real-time analytics and up-to-date data using API.',
-            titre: 'Crypto-currency website',
-            img: currency,
-            link : "https://currency-app-weld.vercel.app/",
+            paragraph: 'EvenXt is a powerful event management system designed to streamline the entire process. ',
+            titre: 'Event management website',
+            img: evenext,
+            link : "https://github.com/Gasbiachraf/Evenext",
 
         },
         {
@@ -45,13 +25,37 @@ export const ProjectsSection = () => {
             link : "https://baby-toys-store.vercel.app/",
 
         },
+        {
+            paragraph: 'Explore the selection of delicious dishes online and discover the creators of these culinary masterpieces.',
+            titre: 'Restaurant website',
+            img: resto,
+            link : "https://lionsresto.netlify.app/",
+        },
+        {
+            paragraph: 'Explore our varied online selection of trendy, high-quality products to suit every style and budget.',
+            titre: 'Store website',
+            img: shopSite,
+            link : "https://storefash.vercel.app/",
+        },
+        {
+            paragraph: 'Medical practice dedicated to your health, offering personalized consultations and attentive care.',
+            titre: 'Medical practice website',
+            img: docteur,
+            link : "https://mediplusag.netlify.app/",
+        },
+        {
+            paragraph: 'Discover current cryptocurrency trends with real-time analytics and up-to-date data using API.',
+            titre: 'Crypto-currency website',
+            img: currency,
+            link : "https://currency-app-weld.vercel.app/",
+        },
     ])
 
     return (
         <section name="projects" className="bg-[#032a31] text-[#a9cfd0] w-full pt-16 overflow-hidden">
             <p className="text-6xl text-[#36a99a] text-center max-[430px]:text-5xl pb-16">Projects</p>
-            <div className='flex justify-center pb-6 '>
-                <div className='flex flex-wrap   gap-y-10 gap-x-10  w-[80vw] max-[430px]:w-[80%] max-[430px]:px-0  '>
+            <div className='flex justify-center  pb-6 '>
+                <div className='flex flex-wrap    gap-y-10 gap-x-10  w-[90%] max-[430px]:w-[80%] max-[430px]:px-0  '>
                     {
                         projects.map((element, index) =>
                             <>
@@ -60,7 +64,7 @@ export const ProjectsSection = () => {
                                     target='blank'
                                     data-aos="fade-up"
                                     data-aos-duration="2000"
-                                    className="max-w-lg hover:bg-transparent w-[48%] max-[430px]:w-[100%] bg-[#032a31] shadow-lg shadow-[#36a99a]  border-0 "
+                                    className="max-w-lg hover:bg-transparent w-[31%] max-[430px]:w-[100%] bg-[#032a31] shadow-lg shadow-[#36a99a]  border-0 "
                                     imgAlt="Meaningful alt text for an image that is not purely decorative"
                                     imgSrc={element.img}
                                 >
